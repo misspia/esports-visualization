8e109618d746e34a33bcf47335e284fc75f551f7
msajaffe <sibtain_95@hotmail.com>
2017-01-01 19:27:02 -0500

    'scraper'

---
+++ scraper/reddit-scraper.js
@@ -0,0 +1,10 @@
+const snoowrap = require('snoowrap');
+
+// Alternatively, just pass in a username and password for script-type apps.
+const otherRequester = new snoowrap({
+  userAgent: 'script/javascript',
+  clientId: 'IS5vnihXIbWw-A',
+  clientSecret: 'failwY5mpnI4i1bdxT_MpYBCBbEe',
+  username: 'yessplease',
+  password: 'firetruck321'
+});
