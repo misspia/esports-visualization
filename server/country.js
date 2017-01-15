f8d5170cf5008967b68cf035b9889414a8d00a4c
User <sibtain_95@hotmail.com>
2017-01-15 04:54:56 -0500

    f u pia

--- server/country.js
+++ server/country.js
@@ -3,16 +3,16 @@ var _ = require('underscore')
 var codes = JSON.parse(fs.readFileSync('./country-codes.json', 'utf8'));
 
 var dota = JSON.parse(fs.readFileSync('./dota.json', 'utf8'));
-var league = JSON.parse(fs.readFileSync('./league.json', 'utf8'));
-var cs = JSON.parse(fs.readFileSync('./csgo.json', 'utf8'));
-var sc = JSON.parse(fs.readFileSync('./sc2.json', 'utf8'));
+//var league = JSON.parse(fs.readFileSync('./league.json', 'utf8'));
+//var cs = JSON.parse(fs.readFileSync('./csgo.json', 'utf8'));
+//var sc = JSON.parse(fs.readFileSync('./sc2.json', 'utf8'));
 
-var games = [dota, league, cs, sc];
+var games = [dota]//, league, cs, sc];
 var names = ['dota.json', 'league.json', 'csgo.json', 'sc2.json'];
 function findCode(country){
   return _.find(codes, function(obj) { return obj.Country.includes(country) })
 }
-for (var i = 0; i < 4; i++) {
+for (var i = 0; i < 1; i++) {
   for (var j = 0; j < games[i].length; j++) {
     games[i][j].code = findCode(games[i][j].country.trim()).Code;
   }
--- server/index.js
+++ server/index.js
@@ -1,44 +1,72 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var scraperjs = require('scraperjs');
-var urls = ['http://www.esportsearnings.com/games/151-starcraft-ii/top-players']
+var argv = require('minimist')(process.argv.slice(2));
+var urls = ['http://www.esportsearnings.com/tournaments/largest-overall-prize-pools']
 var data = {'dota-2': {}};
 var count = 0;
 var fin = false;
+var ranks = ['gold', 'silver', 'bronze']
 
-
-for (var i = 0; i < 1; i++){
-  for (var j = 4; j < 5; j++) {
-    var url = (j == 0) ? urls[i]: urls[i] + '-x' + j*100;
-    request(url, function(err, resp, body) {
-      $ = cheerio.load(body);
-      $(".detail_box_smooth:contains('Top Player Rankings')").find(".format_row").each(function(){
-        var username = $(this).find("a[href*='players']")[0];
-        data['dota-2'][$(username).text()] = {};
-        redirect = "http://www.esportsearnings.com" + $(username).attr('href');
-        scraperjs.StaticScraper.create(redirect)
-        .scrape(function($) {
-          return $(".info_box").map(function() {
+for (var j = argv._[0]; j < argv._[0]+1; j++) {
+  var url = (j == 0) ? urls[0]: urls[0] + '-x' + j*100;
+  request(url, function(err, resp, body) {
+    $ = cheerio.load(body);
+    $(".detail_box_smooth:contains('Largest Overall Prize Pools in eSports')").find(".format_row").each(function(){
+      var username = $(this).find("a[href*='tournaments']")[0];
+      redirect = "http://www.esportsearnings.com" + $(username).attr('href');
+      scraperjs.StaticScraper.create(redirect)
+      .scrape(function($) {
+        return $("body").map(function() {
+          var winners = [];
+          for (var i = 0; i < 3; i++) {
+            if (winners.length === 3) return;
+            var lol = '.tournament_medalist_box .player_earning_'+ranks[winners.length];
+            var team = $(lol).find('.tournament_medalist_name .tournament_team_medalist_name').text() == "" ? "SOLO": $(lol).find('.tournament_medalist_name .tournament_team_medalist_name').text();
+            var names = [];
+            $(lol).find('.tournament_medalist_name .player_team_medalist_link').each(
+              function(){
+                if (names.length == 5) return;
+                names.push({
+                  link: 'http://www.esportsearnings.com' + $(this).attr('href'),
+                  username: $(this).text()
+                })
+              });
+              winners.push({
+                rank: $(lol).find('.tournament_medalist_rank').text(),
+                team: team,
+                earnings: $(lol).find('.tournament_medalist_prize').text(),
+                names: names
+              })
+            }
             return {
-              name: $(this).find('.format_row:contains("Name:") .info_text_value').text(),
-              age:  $(this).find('.format_row:contains("Age:") .info_text_value').text(),
-              totalEarnings:  $(this).find('.format_row:contains("Total Prize Money Earned:") .info_text_value span').text(),
-              numTournaments:  $(this).find('.format_row:contains("Total Prize Money Earned:") .info_text_value').text(),
-              country:         $(this).find('.info_box .info_country').text(),
-              worldRanking:     $(this).find('.info_box .format_row:contains("World Ranking:") .info_text_value').text()
+              title:  $(this).find('.info_box .info_box_title').text(),
+              location: $(this).find('.info_box .format_row:contains("Location:") .info_text_value').text(),
+              date: $(this).find('.info_box .format_row:contains("Date:") .info_text_value').text(),
+              game: $(this).find('.info_box .format_row:contains("Game:") .info_text_value a').text(),
+              prizePool: $(this).find('.info_box .format_row:contains("Prize Pool:") .info_text_value').text(),
+              currency: $(this).find('.info_box .format_row:contains("Currency:") .info_text_value').text(),
+              winners: winners
             };
           }).get();
         })
         .then(function(news) {
           if (news.length != 0) {
-            news[0].numTournaments = news[0].numTournaments.split('From')[1].replace(/\D/g, '');
-            //data['dota-2'][username] = news[0];
-            console.log(news[0]);
+            var tourney = news[0];
+            if (tourney.game.toLowerCase().includes('dota') || tourney.game.toLowerCase().includes('league') || tourney.game.toLowerCase().includes('counter') || tourney.game.toLowerCase().includes('starcraft')) {
+              for (var i = 0; i < tourney.winners.length; i++) {
+                tourney.winners[i].rank = tourney.winners[i].rank.replace(/\D/g, '').slice(0,1);
+                tourney.winners[i].earnings = tourney.winners[i].earnings.replace(/\D/g, '');
+              }
+              tourney.prizePool = tourney.prizePool.replace(/\D/g, '');
+              //news[0].numTournaments = news[0].numTournaments.split('From')[1].replace(/\D/g, '');
+              //data['dota-2'][username] = news[0];
+              console.log(JSON.stringify(news[0]));
+            }
           }
         })
       }) //foreach
     }); //request
   }
-}
 
-//console.log(data['dota-2'][username]);
+  //console.log(data['dota-2'][username]);
